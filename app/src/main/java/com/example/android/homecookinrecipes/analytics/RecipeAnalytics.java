89d847b61d0b60d5bc3618d749578391c5747a45
Xialong Deng <xdeng9@gmail.com>
2017-04-07 23:46:41 -0700

    Add Google Analytics

---
+++ app/src/main/java/com/example/android/homecookinrecipes/analytics/RecipeAnalytics.java
@@ -0,0 +1,9 @@
+package com.example.android.homecookinrecipes.analytics;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.Tracker;
+
+public class RecipeAnalytics extends Application{
+    private Tracker mTracker;
+}
